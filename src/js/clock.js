2ede8c6fc739dc2df3ac24c744dd5c8d6abab8b7
MrWillCom <mr.will.com@outlook.com>
2022-04-25 21:51:47 +0800

    Add second to clock

--- src/js/clock.js
+++ src/js/clock.js
@@ -5,11 +5,20 @@ const updateClocks = () => {
             el.innerHTML = current.getHours()
         }
     })
+
     document.querySelectorAll('#clock-minute').forEach((el) => {
         const current = new Date()
         const minute = current.getMinutes() < 10 ? '0' + current.getMinutes() : current.getMinutes().toString()
         if (el.innerHTML != minute) {
             el.innerHTML = minute
+        }
+    })
+
+    document.querySelectorAll('#clock-second').forEach((el) => {
+        const current = new Date()
+        const second = current.getSeconds() < 10 ? '0' + current.getSeconds() : current.getSeconds().toString()
+        if (el.innerHTML != second) {
+            el.innerHTML = second
         }
     })
 }
